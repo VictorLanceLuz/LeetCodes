5b25f74d236f7f3206d9c475d1875d80851bcd26
Victor Luz <victorlanceluz@outlook.com>
2023-10-15 23:40:47 -0700

    34

--- 34.find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -5,10 +5,25 @@
  */
 
 // @lc code=start
+#include <iostream>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+        // We will perform binary search to get O(log n) space-time complexity
+        vector<int> ans = {};
+        ans.push_back(9);
         
+        
+
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << endl;
+
+        }
+
+        return ans;
     }
 };
 // @lc code=end
