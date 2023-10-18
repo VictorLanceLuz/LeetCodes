1a8e25d1127b0de6e2bfc63dee45b317e6896baf
VictorLanceLuz <victorlanceluz@outlook.com>
2023-10-17 22:21:47 -0700

    4 BUT NAIVE VERSION ONLY for now

--- 26.remove-duplicates-from-sorted-array.cpp
+++ 26.remove-duplicates-from-sorted-array.cpp
@@ -5,10 +5,25 @@
  */
 
 // @lc code=start
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        int k = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {   // If previous is not same as current. Update
+                nums[k] = nums[i];  // This part changes previous.
+                k++;
+            }
+            
+        }
         
+        return k; 
     }
 };
 // @lc code=end
--- 4.median-of-two-sorted-arrays.cpp
+++ 4.median-of-two-sorted-arrays.cpp
@@ -5,11 +5,39 @@
  */
 
 // @lc code=start
+#include <iostream>
+#include <vector>
+using namespace std;
+#define NAIVE 1
 class Solution {
 public:
+    #ifdef NAIVE 
+    // I will just do the naive O(m+n) solution. Come b
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> combinedArray;
+        for (int i = 0; i < nums1.size(); i++) {
+            combinedArray.push_back(nums1[i]);
+        }
+        for (int i = 0; i < nums2.size(); i++) {
+            combinedArray.push_back(nums2[i]);
+        }
+
+        // Sort the combined array
+        sort(combinedArray.begin(), combinedArray.end());
+
+        int n = combinedArray.size();
+        if (n % 2 == 0) // Divisible by 2 = Even
+            return ((double)combinedArray[n / 2 - 1] + (double)combinedArray[n / 2]) / 2.0;
         
+        else // Odd
+            return (double)combinedArray[n / 2];
+
+        
+
     }
+    #endif
+
+    #ifdef BINARY_SEARCH_METHOD
 };
 // @lc code=end
 
