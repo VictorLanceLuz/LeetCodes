9f392b96d82a3899c03fb13c1c81e0c403fae3c0
VictorLanceLuz <victorlanceluz@outlook.com>
2023-10-17 21:59:54 -0700

    update question 34

--- 34.find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -6,22 +6,55 @@
 
 // @lc code=start
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void modifiedBinarySearch(int left, int right, vector<int> &nums, int target, bool findFirstPos, vector<int> &ans) {
+        
+        while (left <= right) 
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (findFirstPos == true) 
+                {
+                    ans[0] = mid;       // update firstPos
+                    right = mid - 1;    // Keep searching on left side.
+                }
+                else
+                {
+                    ans[1] = mid;       // update lastPos
+                    left = mid + 1;     // Keep searching on right side. 
+                }
+            }
+
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        
+    }
+
 class Solution {
 public:
+    
     vector<int> searchRange(vector<int>& nums, int target) {
         // We will perform binary search to get O(log n) space-time complexity
-        vector<int> ans = {};
-        ans.push_back(9);
-        
-        
+        int firstPos = -1;
+        int lastPos = -1;
+        int right = nums.size() - 1;
+        int left = 0;
+        vector<int> ans = {firstPos, lastPos};
 
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << endl;
+        
+        // Use binary search to find first position (bool = true) or last position (bool = false)
+        modifiedBinarySearch(left, right, nums, target, true, ans);
+        modifiedBinarySearch(left, right, nums, target, false, ans);
 
-        }
+        
 
         return ans;
     }
